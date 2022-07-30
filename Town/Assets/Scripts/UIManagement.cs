78d3a83606a003ad0414c66fd19ef0585ffcfd0f
charlie-robison <charlierobison480@gmail.com>
2022-07-30 10:08:45 -1000

    Player can now switch between inventory types by pressing the arrow keys.

--- Town/Assets/Scripts/UIManagement.cs
+++ Town/Assets/Scripts/UIManagement.cs
@@ -8,14 +8,26 @@ public class UIManagement : MonoBehaviour
     public GameObject inventoryCanvas;
 
     private bool enableInventory = false;
+    private Vector2 inventorySelectorDirection;
+    private GameObject currentInventory;
+    private int currentInvIndex;
+    private float switchTimer = 0f;
+    private float timeInterval = 0f;
 
     private void Awake()
     {
         controls = new GameControls();
 
         controls.UI.Inventory.performed += ctx => enableInventory = !enableInventory;
+        controls.UI.SwitchInventory.performed += ctx => inventorySelectorDirection = ctx.ReadValue<Vector2>();
+        controls.UI.SwitchInventory.canceled += ctx => inventorySelectorDirection = Vector2.zero;
     }
 
+    private void Start()
+    {
+        currentInvIndex = 4;
+        currentInventory = inventoryCanvas.transform.GetChild(currentInvIndex).gameObject;
+    }
 
     void OnEnable()
     {
@@ -34,6 +46,36 @@ public class UIManagement : MonoBehaviour
             Time.timeScale = 0f;
             inventoryCanvas.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
+
+            print(currentInvIndex);
+
+            // Checks if the player selected a different inventory.
+            if (inventorySelectorDirection.magnitude >= 0.1f && timeInterval > switchTimer)
+            {
+                // Checks if the right arrow or left arrow was pressed.
+                if (inventorySelectorDirection.x == 1f && currentInvIndex < 5)
+                {
+                    // Sets the current inventory to false.
+                    currentInventory.SetActive(false);
+                    currentInvIndex++;
+
+                    // Updates the current inventory.
+                    currentInventory = inventoryCanvas.transform.GetChild(currentInvIndex).gameObject;
+                    currentInventory.SetActive(true);
+                }
+                else if (inventorySelectorDirection.x == -1f && currentInvIndex > 4)
+                {
+                    // Sets the current inventory to false.
+                    currentInventory.SetActive(false);
+                    currentInvIndex--;
+
+                    // Updates the current inventory.
+                    currentInventory = inventoryCanvas.transform.GetChild(currentInvIndex).gameObject;
+                    currentInventory.SetActive(true);
+                }
+
+                switchTimer = timeInterval + 50f;
+            }
         }
         else
         {
@@ -46,5 +88,7 @@ public class UIManagement : MonoBehaviour
     private void Update()
     {
         checkUI();
+
+        timeInterval++;
     }
 }
