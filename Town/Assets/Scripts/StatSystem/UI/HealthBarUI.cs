5ac2086766988298bb1865b9f947a9392f4e7e3b
charlie-robison <charlierobison480@gmail.com>
2022-08-29 14:49:10 -1000

    Allows health bar UI to change colors based on the value of the hp.

--- Town/Assets/Scripts/StatSystem/UI/HealthBarUI.cs
+++ Town/Assets/Scripts/StatSystem/UI/HealthBarUI.cs
@@ -8,6 +8,9 @@ public class HealthBarUI : MonoBehaviour
     [SerializeField]
     private Slider slider;
 
+    [SerializeField]
+    private Image fillBar;
+
     public void setHealthUI(float newHealth)
     {
         slider.value = newHealth;
@@ -16,6 +19,25 @@ public class HealthBarUI : MonoBehaviour
     public void setMaxHealthUI(float newMaxHealth)
     {
         slider.maxValue = newMaxHealth;
+    }
+
+
+    private void Update()
+    {
+        float sliderFraction = (slider.value / slider.maxValue);
+
+        if (sliderFraction >= 0.5f)
+        {
+            fillBar.color = Color.green;
+        }
+        else if (sliderFraction <= 0.5f && sliderFraction >= 0.25f)
+        {
+            fillBar.color = Color.yellow;
+        }
+        else if (sliderFraction <= 0.25f)
+        {
+            fillBar.color = Color.red;
+        }
     }
 }
 
